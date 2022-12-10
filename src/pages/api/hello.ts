b7589811ecd80069c871f9bb4e342f2a69372144
Tunde Kadiri <tundek.@Tundes-MacBook-Pro-no1.local>
2022-12-09 18:52:40 -0700

    add prisma

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, User } from "@prisma/client";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(res: NextApiResponse<User[] | Data>) {
+  const prisma = new PrismaClient();
+
+  async function main() {
+    // Connect the client
+    await prisma.$connect();
+    // ... you will write your Prisma Client queries here
+
+    await prisma.user.create({
+      data: {
+        firstname: "John",
+        lastname: "Doe 3",
+        email: "John3@gmail.com",
+      },
+    });
+  }
+
+  main()
+    .then(async () => {
+      const allUsers = await prisma.user.findMany();
+      res.status(200).json(allUsers);
+
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      res.status(401).json({ name: "Internal Server Error" });
+      process.exit(1);
+    });
 }
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export enum Rating {
+  GOOD = "GOOD",
+  BAD = "BAD",
+  NOT_RATED = "NOT_RATED",
+}
