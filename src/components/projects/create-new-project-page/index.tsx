e65604355f6be8e9cc81b594629a33b7e5c8341c
David Osemwegie <dosemwegie@gmail.com>
2022-12-10 01:43:28 -0700

    started creating project form

--- src/components/projects/create-new-project-page/index.tsx
+++ src/components/projects/create-new-project-page/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {} from 'react-hook-form'
 
 const CreateProject = () => {
     return (
         <div>
-        <h1>Create new projects</h1>
+        <h1>Create a new project</h1>
         </div>
     )
 }
