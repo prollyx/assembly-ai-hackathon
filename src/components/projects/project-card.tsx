a8a0c5d8215d1b86d92eb3b0a2fa6d65fb08848a
David Osemwegie <dosemwegie@gmail.com>
2022-12-10 05:37:24 -0700

    navigate to project details page

--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React, {FC} from 'react'
 
 export interface ProjectCardProps {
@@ -8,10 +9,12 @@ export interface ProjectCardProps {
 
 const ProjectCard: FC<ProjectCardProps> = ({ description, id, name}) => {
     return (
+        <Link href={`/projects/${id}`}>
         <div className='project-card'>
             <h2 className='mb-2'>{name}</h2>
             <p className='project-card-description'>{description}</p>
         </div>
+        </Link>
     )
 }
 
--- src/pages/projects/[id]/index.tsx
+++ src/pages/projects/[id]/index.tsx
@@ -1,9 +1,15 @@
 import {PageLayout} from "../../../components/page-layout";
+import { useRouter } from 'next/router'
+
 
 export default function ProjectPage() {
 
+  const router = useRouter()
+  const { id } = router.query
+
   return (
     <PageLayout>
+      <h1>{id}</h1>
         <h2>Project Page</h2>
     </PageLayout>
   );
